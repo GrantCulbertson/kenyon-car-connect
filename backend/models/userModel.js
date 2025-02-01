dcd3193d59fbf6d009093df5e2fa78ef89346c2f
GrantCulbertson <culbertsongrant@gmail.com>
2025-02-01 19:19:19 +0000

    Changes driver_status to has_car

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,5 +5,12 @@ let db = require('../db');
 
 // ----------------------- DEFINE USER CLASS ------------------------//
 class user {
-    
+    constructor({id, name , email, age, gender, has_car}){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.has_car = has_car;
+    }
 }
