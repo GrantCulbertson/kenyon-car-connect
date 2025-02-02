7b15d787d36d8b4d425295ef798042b5fe36241d
GrantCulbertson <culbertsongrant@gmail.com>
2025-02-02 22:56:12 +0000

    adding sign up page

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,11 @@ const userController = require('../controllers/userController');
 //----------------------- DEFINE USER ROUTES ------------------------//
 
 
+//Route to signup page
+router.get("/UserSignup", (req, res) => {
+    res.render("signup");
+});
+
 //Route to create a user
 router.post("/CreateUser", userController.addUser);
 
